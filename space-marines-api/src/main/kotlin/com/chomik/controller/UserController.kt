7c025c51db7fbf37ec55556db31e91c08e3929bf
maksgirya <girya@lanit.ru>
2024-11-25 23:20:14 +0300

    GET /api/ping

--- space-marines-api/src/main/kotlin/com/chomik/controller/UserController.kt
+++ space-marines-api/src/main/kotlin/com/chomik/controller/UserController.kt
@@ -1,14 +1,21 @@
 package com.chomik.controller
 
-
-import jakarta.ws.rs.*
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import jakarta.ws.rs.core.Response
-import jakarta.persistence.Persistence
 
-@Path("/users")
+
+@Path("/ping")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class UserController {
+open class UserController {
+
+
+    @GET
+    fun getUsers(): String {
+        return "pong"
+    }
 
 }
