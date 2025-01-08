54b4eca973aea3c5a219be427e56b9aba175a74a
rsushe <sushenkoroman2003@gmail.com>
2025-01-08 21:55:34 +0300

    Fixes for production

--- space-marines-ejb/src/main/kotlin/com/chomik/domain/QueryParams.kt
+++ space-marines-ejb/src/main/kotlin/com/chomik/domain/QueryParams.kt
@@ -2,6 +2,7 @@ package com.chomik.domain
 
 import com.chomik.domain.enums.SortColumn
 import com.chomik.domain.enums.SortOrder
+import java.io.Serializable
 
 data class QueryParams(
     val sortBy: List<SortColumn>?,
@@ -9,4 +10,4 @@ data class QueryParams(
     val filters: List<Filter>?,
     val page: Int,
     val size: Int,
-)
+): Serializable
