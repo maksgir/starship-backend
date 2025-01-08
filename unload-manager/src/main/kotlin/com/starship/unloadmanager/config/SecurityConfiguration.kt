1ac9e17057c6d8624458e0487abaf2679c8a6a28
rsushe <sushenkoroman2003@gmail.com>
2025-01-08 22:33:22 +0300

    Fixes for production

--- unload-manager/src/main/kotlin/com/starship/unloadmanager/config/SecurityConfiguration.kt
+++ unload-manager/src/main/kotlin/com/starship/unloadmanager/config/SecurityConfiguration.kt
@@ -2,9 +2,13 @@ package com.starship.unloadmanager.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +16,21 @@ class SecurityConfiguration {
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        return http
-            .csrf { csrfConfigurer -> csrfConfigurer.disable() }
-            .authorizeHttpRequests { security ->
-                security.anyRequest().permitAll()
+        http.invoke {
+            authorizeRequests {
+                authorize(anyRequest, permitAll)
             }
-            .build()
+            cors {
+                configurationSource = CorsConfigurationSource {
+                    val configuration = CorsConfiguration().applyPermitDefaultValues()
+                    configuration.allowedOrigins = listOf("http://localhost", "https://se.ifmo.ru")
+                    configuration.addAllowedMethod(HttpMethod.POST)
+                    configuration
+                }
+            }
+            csrf { disable() }
+        }
+
+        return http.build()
     }
 }
